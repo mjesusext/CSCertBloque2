9842459c404032135fba4421e0aa673aac183f20
Manuel Jesus Extremera Extremera <manuel.extremera@scmspain.com>
2019-02-04 13:46:54 +0100

    Eliminar referencias a modulos anteriores.
    Añadir a solución de modulo 6 las clases de la teoría después de los ejemplos. Así tenemos clases para testear los ejercicios

---
+++ Modulo6/ClasesEjemploTeoria/IFuncionario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo6
+{
+    interface IFuncionario
+    {
+        string Nombre { get; set; }
+        string Apellidos { get; set; }
+
+        void CotejarDocumentos();
+    }
+}
--- Modulo6/Program.cs
+++ Modulo6/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Modulo5;
 
 namespace Modulo6
 {
@@ -12,9 +11,18 @@ namespace Modulo6
         static void Main(string[] args)
         {
             #region Ejercicio 1
-
+            Persona a1 = new Persona("Carlos", "Rodríguez", DateTime.Now);
+            Persona a2 = new Persona("Carlos", "Rodríguez", DateTime.Now);
+            int i1 = 1, i2 = 1;
+            string s1 = "Hola", s2 = "Hola";
+            
+            //Como todos los tipos se usan y son informados en método, lo infiere
+            Console.WriteLine("Comparación de personas: {0}", Igualdad(a1, a2));
+            Console.WriteLine("Comparación de enteros: {0}", Igualdad(i1, i2));
+            Console.WriteLine("Comparación de strings: {0}", Igualdad(s1, s2));
             #endregion
 
+            Console.ReadLine();
         }
 
         //Ejercicio 1
